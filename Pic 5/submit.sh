b6fd615552355b434d7a12068b2fe6e3d988a5c7
Mathieu Vallée <mathieu.vallee1998@gmail.com>
2026-03-26 13:49:26 +0100

    submit

--- Pic 5/submit.sh
+++ Pic 5/submit.sh
@@ -4,8 +4,8 @@
 #SBATCH --output=res_Pic_5.txt
 #
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=256
-#SBATCH --time=2:00:00
+#SBATCH --cpus-per-task=100
+#SBATCH --time=20:00
 #SBATCH --mem-per-cpu=10
 
 srun julia --threads 256 Pic 5/algo_pic5_link_parallel_all_v_better.jl
