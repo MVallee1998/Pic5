fb9fc506ac8018324340742136df591e6030d241
Mathieu Vallée <mathieu.vallee1998@gmail.com>
2026-04-01 11:35:59 +0200

    change for right number of threads and 2 days

--- Pic 5/submit.sh
+++ Pic 5/submit.sh
@@ -4,8 +4,8 @@
 #SBATCH --output=res_Pic_5_7-10.txt
 #
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
-#SBATCH --time=20:00
+#SBATCH --cpus-per-task=128
+#SBATCH --time=2-00:00
 #SBATCH --mem=100G
 
-srun julia --threads=100 ./algo_pic5_link_parallel_all_v_better.jl
+srun julia --threads=128 ./algo_pic5_link_parallel_all_v_better.jl
